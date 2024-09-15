7941bf1423c736c54fe5356434009c44155901cf
arpit-sys <shreeraghuvanshi7@gmail.com>
2024-09-15 20:09:38 +0530

    Adding Dark mode to whole APP

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,12 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+
+  const[mode, setMode] = useState('light') //Tells whether dark mode is enabled or not
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#202634'
+      document.body.style.color = 'white'
+
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white'
+      document.body.style.color = 'black'
+    }
+  }
+
+
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About Us"/>
+      <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze below" />
         {/* <About/> */}
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -19,13 +19,13 @@ export default function TextForm(props) {
         setText(newText);
     }
 
-    const handleReverse = ()=> {
+    const handleReverse = () => {
         let newText = text.split('').reverse().join('');
         setText(newText)
     }
 
-    const handleCapitaliseFirstLetter = () =>{
-        let newText = text.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase()+ word.slice(1)).join(' ');
+    const handleCapitaliseFirstLetter = () => {
+        let newText = text.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
         setText(newText);
     }
 
@@ -42,19 +42,20 @@ export default function TextForm(props) {
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
                     <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                    {console.log("Color changed")}
                 </div>
                 <button className="btn btn-success mx-2" onClick={handleUpClick}>Covert to Uppercase</button>
                 <button className="btn btn-success mx-2" onClick={handleLoClick}>Covert to Lowercase</button>
                 <button className="btn btn-success mx-2" onClick={handleCapitaliseFirstLetter}>Capitalize First Letter</button>
                 <button className="btn btn-success mx-2" onClick={handleReverse}>Reverse</button>
                 <button className="btn btn-success mx-2" onClick={handleClearText}>Clear</button>
-                
+
 
             </div>
             <div className="container my-2">
                 <h2>Your text summary</h2>
                 <p>{text.split(" ").length} words and {text.length} characters</p>
-                <p>{0.008*text.split(" ").length} minutes to read</p>
+                <p>{0.008 * text.split(" ").length} minutes to read</p>
                 <h2>Preview</h2>
                 <p>{text}</p>
 
