25f9cbfad58b03513e77fca9ab315db1dd0ef3f1
arpit-sys <shreeraghuvanshi7@gmail.com>
2024-09-28 22:32:51 +0530

    Added Alert Feature to this App, Also added Rect Router Feature to this App

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -3,32 +3,56 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
 function App() {
 
-  const[mode, setMode] = useState('light') //Tells whether dark mode is enabled or not
+  const[mode, setMode] = useState('light'); //Tells whether dark mode is enabled or not
+  const[alert, setAlert] = useState(null); 
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if(mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor = '#202634'
-      document.body.style.color = 'white'
-
-    }else{
+      document.body.style.backgroundColor = '#202634';
+      document.body.style.color = 'white';
+      showAlert("Dark Mode has been enabled", "success");
+    } else {
       setMode('light');
-      document.body.style.backgroundColor = 'white'
-      document.body.style.color = 'black'
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+      showAlert("Light Mode has been enabled", "success");
     }
-  }
-
+  };
 
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
-      <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below" mode={mode}/>
-        {/* <About/> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -7,27 +7,36 @@ export default function TextForm(props) {
         console.log("Uppercase was clicked " + text);
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Text converted to Upper case!", "success");
     }
 
     const handleLoClick = () => {
         console.log("Lowercase was clicked " + text);
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Text converted to Lower case!", "success");
+
     }
 
     const handleClearText = () => {
         let newText = ("");
         setText(newText);
+        props.showAlert("Text has been cleared!", "success");
+
     }
 
     const handleReverse = () => {
         let newText = text.split('').reverse().join('');
         setText(newText)
+        props.showAlert("Text converted to Reverse!", "success");
+
     }
 
     const handleCapitaliseFirstLetter = () => {
         let newText = text.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
         setText(newText);
+        props.showAlert("First Letter of your text has been capitalized!", "success");
+
     }
 
     const handleOnChange = (event) => {
